2b19ccf424de42fb28b6cb6fe287a3492cca9cf4
Júlia Brito <julialbritto@gmail.com>
2022-05-09 21:51:58 -0300

    commit

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,41 @@
+import { List, ListRowRenderer } from 'react-virtualized';
+import { ProductItem } from './ProductItem';
+
+interface SearchResultProps {
+  totalPrice: number;
+  results: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>
+  onAddToWishlist: (id: number) => void;
+}
+
+export function SearchResults({ totalPrice, results, onAddToWishlist }: SearchResultProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]} 
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    );
+  } 
+
+  return (
+    <div>
+      <h2>Total: R$ {totalPrice}</h2>
+      
+      <List 
+        width={900}
+        height={300}
+        rowHeight={30}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+    </div>
+  );
+}
